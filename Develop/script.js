a83593e2a641968d183626e4c6ed1e6f2719b674
JordynEnos <jordynenos11@gmail.com>
2023-03-10 11:11:01 -0800

    added html, js, css.

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Define the character sets for each type of character
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+
+
+
+
+
