c0164de49c88e8ebbb8e016932076761abf6b6b7
JordynEnos <jordynenos11@gmail.com>
2023-03-10 11:16:17 -0800

    Defined the character sets for each type of character. Defined a function to prompt the user for a password length between 8-128 characters.

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,16 @@ var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericChars = "0123456789";
 var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
+// Define a function to prompt the user for a password length between 8 and 128 characters
+function getPasswordLength() {
+  var length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters):"));
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return getPasswordLength();
+  }
+  return length;
+}
+
 
 
 
