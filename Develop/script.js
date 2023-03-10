5eec577d7eb81e3ebca5b47b25a0be5c4be075e8
JordynEnos <jordynenos11@gmail.com>
2023-03-10 11:29:13 -0800

    Defined a function to prompt the user for the types of characters to include in the password.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Define the character sets for each type of character
+// Defined the character sets for each type of character
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericChars = "0123456789";
 var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-// Define a function to prompt the user for a password length between 8 and 128 characters
+// Defined a function to prompt the user for a password length between 8 and 128 characters
 function getPasswordLength() {
   var length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters):"));
   if (isNaN(length) || length < 8 || length > 128) {
@@ -17,7 +17,21 @@ function getPasswordLength() {
   return length;
 }
 
+// Defined a function to prompt the user for the types of characters to include in the password
+function getPasswordCriteria() {
+  var lowercase = confirm("Include lowercase letters?");
+  var uppercase = confirm("Include uppercase letters?");
+  var numeric = confirm("Include numeric characters?");
+  var special = confirm("Include special characters?");
+  return {
+    lowercase: lowercase,
+    uppercase: uppercase,
+    numeric: numeric,
+    special: special
+  };
+}
 
+ 
 
 
 
